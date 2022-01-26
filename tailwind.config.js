9a22b44448399a98079416bbf8124f63ba68b63b
manuelsavino <manuelsavino@gmail.com>
2022-01-26 10:42:32 -0500

    upgrade tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./src/pages/*.js', './src/components/**/*.js'],
+  content: ['./src/pages/*.js', './src/components/**/*.js'],
   theme: {
     colors: {
       blue: colors.blue,
       red: colors.red,
       white: colors.white,
       black: colors.black,
-      gray: colors.trueGray,
+      gray: colors.neutral,
     },
   },
 };
